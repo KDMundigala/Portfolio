1df45a61617a104f23a1b72b2cebbc1616b2d22d
Kasun Dilshan <140871162+KDMundigala@users.noreply.github.com>
2025-10-08 07:22:49 +0530

    update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -118,7 +118,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1HqL6xkITWWH_hIvQyTUeZEIyT2kopger/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1HqL6xkITWWH_hIvQyTUeZEIyT2kopger/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary 
                            hover:bg-primary/10 transition-colors duration-300 
                            flex items-center gap-2"
